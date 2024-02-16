c6d1efb56d97761fe2848fa96006bf50752a7613
Filipe Brochier <filipe.brochier@gmail.com>
2024-02-16 13:24:29 -0300

    feat: implements get_charts_text()

--- src/helpers.py
+++ src/helpers.py
@@ -6,3 +6,15 @@ def get_num_words(text):
 def get_text(path):
   with open(path) as f:
     return f.read()
+
+
+def get_chars_dict(text):
+  letter_dict = {}
+  for letter in text:
+    lowered = letter.lower()
+    if lowered in letter_dict:
+      letter_dict[lowered] += 1
+    else:
+      letter_dict[lowered] = 1
+  return letter_dict
+
